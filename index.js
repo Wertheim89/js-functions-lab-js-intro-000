99997d7c7a95f9503caac81f62d2277c363787bd
Thomas Varo <zalipsn@gmail.com>
2019-07-23 14:49:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ function happyHolidays() {return "Happy holidays!"}
 function happyHolidaysTo(name) {return "Happy holidays, " + `${name}` + "!"; }
 
 function happyHolidayTo(holiday,name) {return "Happy " + `${holiday}` + ", " + `${name}` + "!";}
+
+function holdiayCountdown(holiday,days) {return "It's " + `${days}` + "days until" + `${holiday}` + "!" ;}
+
