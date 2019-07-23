4a144c25f62389617dc5068bbf5583e116e2bbff
Thomas Varo <zalipsn@gmail.com>
2019-07-23 14:46:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // write your code below!
 function happyHolidays() {return "Happy holidays!"}
 
-function happyHolidaysTo(holiday,name) {return "Happy " + `${holiday}` + "," + `${name}` + "!";}
+function happyHolidaysTo(name) {return "Happy holidays, " + `${name}` + "!"; }
+
+function happyHolidayTo(holiday,name) {return "Happy " + `${holiday}` + "," + `${name}` + "!";}
